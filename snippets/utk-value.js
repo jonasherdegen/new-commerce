137f749334820cb881a3a89123718da751c4d597
Jonas <43905734+jonasherdegen@users.noreply.github.com>
2023-05-23 12:28:15 +0200

    Create utk-value.js

---
+++ snippets/utk-value.js
@@ -0,0 +1,38 @@
+// Capture hubspotutk on form submission
+function getCookieValue() {
+  let cookies = document.cookie
+    .split(";")
+    .map((cookie) => cookie.split("="))
+    .reduce(
+      (accumulator, [key, value]) => ({
+        ...accumulator,
+        [key.trim()]: decodeURIComponent(value)
+      }),
+      {}
+    );
+
+  let utk = cookies["hubspotutk"] || "";
+
+  let utkForm = document.getElementById("hubspotutk");
+  if (utkForm) {
+    utkForm.value = utk;
+  }
+
+  let pageName = document.title;
+  let pageURL = window.location.href;
+
+  let pageNameForm = document.getElementById("page-name");
+  if (pageNameForm) {
+    pageNameForm.value = pageName;
+  }
+  let pageURLForm = document.getElementById("page-url");
+  if (pageURLForm) {
+    pageURLForm.value = pageURL;
+  }
+}
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    getCookieValue();
+  }, 1000);
+});
